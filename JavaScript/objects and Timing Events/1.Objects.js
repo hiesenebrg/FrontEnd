ae96f8c97717f5ac70ba5995ee5c01cab58e639e
hiesenebrg <adarshsingh8008@gmail.com>
2022-10-10 10:38:33 +0530

    Objects added ib Objects and Timing Events folder

---
+++ JavaScript/objects and Timing Events/1.Objects.js
@@ -0,0 +1,13 @@
+var student = {
+    name:"abc" , 
+    roll : 135 ,
+     marks:90
+    };
+    console.log(student);
+    student.sem = 5;
+console.log(student);
+// thus there is no fixed length of the object 
+
+// other methods to create object are'
+// var obj = {}
+// var obj = new Object()
