4a75b7f0059b112bde5e5f97e452e0eeee03ac6d
hiesenebrg <adarshsingh8008@gmail.com>
2022-10-13 11:18:33 +0530

    ScrollToandScrollBy

---
+++ JavaScript/Mini Projects Using Javascirpt/ScrollToandScrollBy.js
@@ -0,0 +1,6 @@
+window.scrollTo(0,1200);
+// this will move the window from 0px location to 1200px location only one time as the 'to' word suggests
+window.scrollBy(0,1200);
+// this will move the window from 0px location to 1200px by 1200px and then 2400px by 1200px and so on every time its clicked
+ window.pageYOffset;
+//  this will give the current hieght pixel 
